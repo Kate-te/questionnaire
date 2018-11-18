41b58e3ad582e144548a1a9fdc1e77933c5dd126
Kate Bohuslavska <belikeme103@gmail.com>
2018-11-18 18:05:20 +0200

    Add questionnaire page

--- src/components/QuestionnairePreview.vue
+++ src/components/QuestionnairePreview.vue
@@ -1,10 +1,41 @@
 <template>
-    
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Questionnaire 1</h3>
+        </div>
+        <div class="table-responsive">
+            <table class="table card-table table-striped table-vcenter">
+                <tbody>
+                <tr>
+                    <td>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu sapien, dictum ut iaculis
+                        non, elementum eget lectus. Duis eget bibendum lorem. Proin non interdum orci, vel accumsan
+                        dui. Etiam varius magna sem, ut varius massa ullamcorper ornare. Pellentesque mollis, velit
+                        vitae viverra rutrum, lorem lacus convallis ipsum, sed tempus felis felis sit amet purus.
+                        Fusce interdum urna ipsum, sed malesuada mauris volutpat id. Phasellus vitae erat imperdiet,
+                        pulvinar elit nec, sodales metus. Mauris mollis luctus dolor, sed commodo orci facilisis id.
+                    </td>
+                    <td class="w-1">
+                        <router-link
+                                :to="{name: 'questionnaire', params: {questionnaireId: 1}}"
+                                class="btn btn-primary">
+                            Accéder
+                        </router-link>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
   export default {
     name: 'questionnaire-preview',
+      props: {
+        questionnaire: Object,
+        // required: true
+      }
   }
 </script>
 
--- src/pages/Questionnaires.vue
+++ src/pages/Questionnaires.vue
@@ -1,20 +1,44 @@
 <template>
     <layout>
-        kokoko
+        <div class="my-3 my-md-5">
+            <div class="container">
+                <div class="page-header">
+                    <h1 class="page-title">
+                        Accueil - Tous Les Questionnaires
+                    </h1>
+                </div>
+                <div class="row row-cards">
+                    <div class="col-lg-12">
+
+                        <ul class="list-group">
+                            <li v-for="item in 10" class="list-group-item">
+                                <questionnaire-preview></questionnaire-preview>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </layout>
 </template>
 
 <script>
-  import Layout from '@/components/Layout'
+  import Layout from '@/components/Layout';
+  import QuestionnairePreview from '@/components/QuestionnairePreview'
 
   export default {
     name: 'questionnaire',
     components: {
       Layout,
+      QuestionnairePreview
     }
   }
 </script>
 
 <style scoped>
-
+    .list-group-item {
+        padding: 0;
+        background-color: transparent;
+    }
 </style>
