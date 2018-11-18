e3c85cb1be06a84c3cf191b15facf11623c3e85a
Kate Bohuslavska <belikeme103@gmail.com>
2018-11-18 21:52:26 +0200

    Add collapse, scroll to selected theme in the sidebar

--- src/main.js
+++ src/main.js
@@ -3,12 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import VeeValidate from 'vee-validate';
+import VeeValidate from 'vee-validate'
+import VueScrollTo from 'vue-scrollto'
+import './filters'
 import 'tabler-ui/dist/assets/css/tabler.css'
 
 Vue.config.productionTip = false
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate)
+
+Vue.use(VueScrollTo, {
+  container: "body",
+  duration: 500,
+  easing: "ease",
+  offset: 0,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+})
 
 new Vue({
   router,
--- src/pages/Questionnaire.vue
+++ src/pages/Questionnaire.vue
@@ -11,15 +11,18 @@
                     <div class="col-lg-4">
                         <div class="row">
                             <div class="col-md-6 col-lg-12">
-                                <theme-sidebar />
+                                <theme-sidebar :themes="getQuestionnaireThemes"
+                                               @collapsed="onToggleCollapse"/>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-8">
                         <theme v-for="(theme, index) in getQuestionnaireThemes"
                                :key="theme.id"
+                               :opened="themeCollapsed[theme.id]"
                                :index-theme="index + 1"
-                               :theme="theme"></theme>
+                               :theme="theme"
+                               @collapsed="onToggleCollapse" />
                     </div>
                 </div>
             </div>
@@ -43,10 +46,21 @@
     data() {
       return {
         idQuestionnaire: this.$route.params.questionnaireId,
+        themeCollapsed: []
       }
     },
     computed: {
       ...mapGetters(['getQuestionnaireThemes']),
+    },
+    created() {
+      this.themeCollapsed = this.getQuestionnaireThemes.reduce((obj, theme) => {
+        obj[theme.id] = false;
+        return obj}, {});
+    },
+    methods: {
+      onToggleCollapse(val) {
+        this.themeCollapsed[val.id] = val.state
+      }
     }
   }
 </script>
